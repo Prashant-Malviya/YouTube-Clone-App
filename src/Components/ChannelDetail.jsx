7baffc76c530bf78008726db9eeb624fadf770eb
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-12-26 23:29:05 +0530

    channel detail fetched through api

--- src/Components/ChannelDetail.jsx
+++ src/Components/ChannelDetail.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { fetchFromAPI } from '../Utils/fetchFromAPI';
 
 function ChannelDetail() {
+  const [channelDetail,setChannelDetail] = useState(null);
+  const {id} = useParams();
+  const [videos,setVideos] = useState([])
+
+  console.log(channelDetail, videos)
+
+  useEffect(()=>{
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data)=>setChannelDetail(data?.items[0]))
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data)=>setVideos(data?.items));
+  },[id])
+
+
   return (
     <div>
-      Channel detail
+    {id}
     </div>
   )
 }
